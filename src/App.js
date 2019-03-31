575df3e9420aa455fb04cf19e5f68c0656b062f7
Powerlated <zjia08@gmail.com>
2019-03-31 14:21:25 -0400

    prettier install & format

--- src/App.js
+++ src/App.js
@@ -3,29 +3,26 @@ import logo from './r6_logo.png';
 import './App.css';
 
 import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
-import "./react-tabs.css";
+import './react-tabs.css';
 
 import Button from '@material-ui/core/Button';
 
-const ColumnHeader = ({
-  onClick,
-  onClickAll
-}) => (
-    <div className="row colheader">
-      <div className="col-xs-1">
-        <h4>#</h4>
-      </div>
-      <div className="col-xs-5">
-        <h4>Name</h4>
-      </div>
-      <div className="col-xs-3 recent">
-        <h4 onClick={onClick} >Last 30 days</h4>
-      </div>
-      <div className="col-xs-3 alltime">
-        <h4 onClick={onClickAll} >All time</h4>
-      </div>
+const ColumnHeader = ({ onClick, onClickAll }) => (
+  <div className="row colheader">
+    <div className="col-xs-1">
+      <h4>#</h4>
     </div>
-  );
+    <div className="col-xs-5">
+      <h4>Name</h4>
+    </div>
+    <div className="col-xs-3 recent">
+      <h4 onClick={onClick}>Last 30 days</h4>
+    </div>
+    <div className="col-xs-3 alltime">
+      <h4 onClick={onClickAll}>All time</h4>
+    </div>
+  </div>
+);
 
 class App extends Component {
   _clickAllTime(e) {
@@ -38,10 +35,8 @@ class App extends Component {
     this.setState(sorted);
   }
 
-
   render() {
     return (
-
       <div className="App">
         <Tabs className="Everything">
           <TabList>
@@ -57,15 +52,22 @@ class App extends Component {
               <img src={logo} className="App-logo" alt="" />
               <h1>TEAM KILL PASS TRACKER</h1>
               <p>
-                The beginnings of an EPIC Rainbow Six Siege (and other multiplayer game) Team Kill Pass system.
-        </p>
+                The beginnings of an EPIC Rainbow Six Siege (and other
+                multiplayer game) Team Kill Pass system.
+              </p>
 
               <h2>Why did I make this?</h2>
-              <p>To be completely <s>hecking</s> honest, I do not know.</p>
-              <p>Actually, to be realer than real, I made this system because I was instpired by a guy named yeboizaidey
-                on Discord to do so. He sorta started a TK Pass "honor system" that just grew into a giant meme.
-                Zaidey also triedd to convince me that making this website was all a big waste of time, but I did not listen
-          because I am always up for a challenge.</p>
+              <p>
+                To be completely <s>hecking</s> honest, I do not know.
+              </p>
+              <p>
+                Actually, to be realer than real, I made this system because I
+                was instpired by a guy named yeboizaidey on Discord to do so. He
+                sorta started a TK Pass "honor system" that just grew into a
+                giant meme. Zaidey also triedd to convince me that making this
+                website was all a big waste of time, but I did not listen
+                because I am always up for a challenge.
+              </p>
               <a
                 className="App-link"
                 href="https://reactjs.org"
@@ -73,7 +75,7 @@ class App extends Component {
                 rel="noopener noreferrer"
               >
                 Learn React
-          </a>
+              </a>
             </header>
           </TabPanel>
           <TabPanel>
@@ -86,11 +88,15 @@ class App extends Component {
             <h2>Any content 2</h2>
           </TabPanel>
           <TabPanel>
-            <Button variant="contained" color="primary">Material</Button>
-            <ColumnHeader onClickAll={this._clickAllTime} onClick={this._clickRecent} />
+            <Button variant="contained" color="primary">
+              Material
+            </Button>
+            <ColumnHeader
+              onClickAll={this._clickAllTime}
+              onClick={this._clickRecent}
+            />
           </TabPanel>
         </Tabs>
-
       </div>
     );
   }
