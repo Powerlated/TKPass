ae92dd54f846673ef32a10350a581c05eb055854
Powerlated <zjia08@gmail.com>
2019-03-31 05:53:40 -0400

    It begins.

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ class App extends Component {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
-            Edit <code>src/App.js</code> and save to reload.
+            The beginnings of an EPIC Rainbow Six Siege (and other multiplayer game) Team Kill Pass system.
           </p>
           <a
             className="App-link"
