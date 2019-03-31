4defe8ca850a235ac01af359a962defd62424a94
Powerlated <zjia08@gmail.com>
2019-03-31 09:04:04 -0400

    Initial changes for making website resemble  product

--- src/App.js
+++ src/App.js
@@ -1,13 +1,21 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import logo from './r6_logo.png';
 import './App.css';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
+import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
+import "./react-tabs.css";
+
+let tabs = (
+  <Tabs className="Everything">
+    <TabList>
+      <Tab>Home</Tab>
+      <Tab>Title 2</Tab>
+    </TabList>
+
+    <TabPanel>
+      <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo"/>
+          <h1>TEAM KILL PASS TRACKER</h1>
           <p>
             The beginnings of an EPIC Rainbow Six Siege (and other multiplayer game) Team Kill Pass system.
           </p>
@@ -20,6 +28,20 @@ class App extends Component {
             Learn React
           </a>
         </header>
+    </TabPanel>
+    <TabPanel>
+      <h2>Any content 2</h2>
+    </TabPanel>
+  </Tabs>
+);
+
+class App extends Component {
+  render() {
+    return (
+
+      <div className="App">
+        {tabs}
+        
       </div>
     );
   }
