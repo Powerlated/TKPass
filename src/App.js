6a74d455ef1f14718a3f83ba82ed25eecf1f31eb
Powerlated <zjia08@gmail.com>
2019-04-01 02:27:30 -0400

    Leaderboard import form

--- src/App.js
+++ src/App.js
@@ -1,46 +1,39 @@
-import React, { Component } from 'react';
-import logo from './r6_logo.png';
-import './App.css';
+import React, { Component } from "react";
+import logo from "./r6_logo.png";
+import "./App.css";
 
-import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
-import './react-tabs.css';
+import { Tab, Tabs, TabList, TabPanel } from "react-tabs";
+import "./react-tabs.css";
 
-import Button from '@material-ui/core/Button';
+import Leaderboard from "./leaderboard/Leaderboard";
+import "./leaderboard/Leaderboard.css";
 
-const ColumnHeader = ({ onClick, onClickAll }) => (
-  <div className="row colheader">
-    <div className="col-xs-1">
-      <h4>#</h4>
-    </div>
-    <div className="col-xs-5">
-      <h4>Name</h4>
-    </div>
-    <div className="col-xs-3 recent">
-      <h4 onClick={onClick}>Last 30 days</h4>
-    </div>
-    <div className="col-xs-3 alltime">
-      <h4 onClick={onClickAll}>All time</h4>
-    </div>
-  </div>
-);
+import { Form, Text } from 'informed';
+
 
 class App extends Component {
-  _clickAllTime(e) {
-    let sorted = this.state.list.sort((a, b) => b.alltime - a.alltime);
-    this.setState(sorted);
-  }
-
-  _clickRecent(e) {
-    let sorted = this.state.list.sort((a, b) => b.recent - a.recent);
-    this.setState(sorted);
-  }
-
   render() {
+    let leaderboard = <Leaderboard />;
+
+    const leaderboardTab = (
+      <TabPanel
+        style={{
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "center"
+        }}
+      >
+        {leaderboard}
+      </TabPanel>
+    );
+
     return (
       <div className="App">
         <Tabs className="Everything">
           <TabList>
-            <Tab>R6S TK PASS</Tab>
+            <Tab>
+              <div id="tabtext">R6S TK PASS</div>
+            </Tab>
             <Tab>Members</Tab>
             <Tab>Staff</Tab>
             <Tab>Leaderboard</Tab>
@@ -62,20 +55,20 @@ class App extends Component {
               </p>
               <p>
                 Actually, to be realer than real, I made this system because I
-                was instpired by a guy named yeboizaidey on Discord to do so. He
+                was inspired by a guy named yeboizaidey on Discord to do so. He
                 sorta started a TK Pass "honor system" that just grew into a
                 giant meme. Zaidey also tried to convince me that making this
                 website was all a big waste of time, but I did not listen
                 because I am always up for a challenge.
               </p>
-              <a
+              {/* <a
                 className="App-link"
                 href="https://reactjs.org"
                 target="_blank"
                 rel="noopener noreferrer"
               >
                 Learn React
-              </a>
+              </a> */}
             </header>
           </TabPanel>
           <TabPanel>
@@ -84,19 +77,15 @@ class App extends Component {
           <TabPanel>
             <h2>Any content 2</h2>
           </TabPanel>
-          <TabPanel>
-            <h2>Any content 2</h2>
-          </TabPanel>
-          <TabPanel>
-            <Button variant="contained" color="primary">
-              Material
-            </Button>
-            <ColumnHeader
-              onClickAll={this._clickAllTime}
-              onClick={this._clickRecent}
-            />
-          </TabPanel>
+
+          {leaderboardTab}
+
+          <TabPanel>REQUESTS</TabPanel>
         </Tabs>
+        <p style={{ color: "gray" }}>
+          This website is completely unrelated and unaffiliated with Rainbow
+          Six, Ubisoft, or any of its properties and IP.
+        </p>
       </div>
     );
   }
--- src/leaderboard/Leaderboard.js
+++ src/leaderboard/Leaderboard.js
@@ -1,28 +1,27 @@
-import React from 'react';
-import classNames from 'classnames';
-import PropTypes from 'prop-types';
-import { withStyles } from '@material-ui/core/styles';
-import Table from '@material-ui/core/Table';
-import TableBody from '@material-ui/core/TableBody';
-import TableCell from '@material-ui/core/TableCell';
-import TableHead from '@material-ui/core/TableHead';
-import TablePagination from '@material-ui/core/TablePagination';
-import TableRow from '@material-ui/core/TableRow';
-import TableSortLabel from '@material-ui/core/TableSortLabel';
-import Toolbar from '@material-ui/core/Toolbar';
-import Typography from '@material-ui/core/Typography';
-import Paper from '@material-ui/core/Paper';
-import Checkbox from '@material-ui/core/Checkbox';
-import IconButton from '@material-ui/core/IconButton';
-import Tooltip from '@material-ui/core/Tooltip';
-import DeleteIcon from '@material-ui/icons/Delete';
-import FilterListIcon from '@material-ui/icons/FilterList';
-import { lighten } from '@material-ui/core/styles/colorManipulator';
+import React from "react";
+import classNames from "classnames";
+import PropTypes from "prop-types";
+import { withStyles } from "@material-ui/core/styles";
+import Table from "@material-ui/core/Table";
+import TableBody from "@material-ui/core/TableBody";
+import TableCell from "@material-ui/core/TableCell";
+import TableHead from "@material-ui/core/TableHead";
+import TablePagination from "@material-ui/core/TablePagination";
+import TableRow from "@material-ui/core/TableRow";
+import TableSortLabel from "@material-ui/core/TableSortLabel";
+import Toolbar from "@material-ui/core/Toolbar";
+import Typography from "@material-ui/core/Typography";
+import Paper from "@material-ui/core/Paper";
+import IconButton from "@material-ui/core/IconButton";
+import Tooltip from "@material-ui/core/Tooltip";
+import DeleteIcon from "@material-ui/icons/Delete";
+import FilterListIcon from "@material-ui/icons/FilterList";
+import { lighten } from "@material-ui/core/styles/colorManipulator";
 
 let counter = 0;
-function createData(name, calories, fat, carbs, protein) {
+function createData(name, passes) {
   counter += 1;
-  return { id: counter, name, calories, fat, carbs, protein };
+  return { id: counter, name, passes };
 }
 
 function desc(a, b, orderBy) {
@@ -46,15 +45,14 @@ function stableSort(array, cmp) {
 }
 
 function getSorting(order, orderBy) {
-  return order === 'desc' ? (a, b) => desc(a, b, orderBy) : (a, b) => -desc(a, b, orderBy);
+  return order === "desc"
+    ? (a, b) => desc(a, b, orderBy)
+    : (a, b) => -desc(a, b, orderBy);
 }
 
 const rows = [
-  { id: 'name', numeric: false, disablePadding: true, label: 'Name' },
-  { id: 'calories', numeric: true, disablePadding: false, label: 'Calories' },
-  { id: 'fat', numeric: true, disablePadding: false, label: 'Fat (g)' },
-  { id: 'carbs', numeric: true, disablePadding: false, label: 'Carbs (g)' },
-  { id: 'protein', numeric: true, disablePadding: false, label: 'Protein (g)' },
+  { id: "name", numeric: false, disablePadding: true, label: "Name" },
+  { id: "passes", numeric: true, disablePadding: false, label: "Passes" }
 ];
 
 class EnhancedTableHead extends React.Component {
@@ -63,29 +61,29 @@ class EnhancedTableHead extends React.Component {
   };
 
   render() {
-    const { onSelectAllClick, order, orderBy, numSelected, rowCount } = this.props;
+    const {
+      onSelectAllClick,
+      order,
+      orderBy,
+      numSelected,
+      rowCount
+    } = this.props;
 
     return (
       <TableHead>
         <TableRow>
-          <TableCell padding="checkbox">
-            <Checkbox
-              indeterminate={numSelected > 0 && numSelected < rowCount}
-              checked={numSelected === rowCount}
-              onChange={onSelectAllClick}
-            />
-          </TableCell>
+          <TableCell padding="checkbox" />
           {rows.map(
             row => (
               <TableCell
                 key={row.id}
-                align={row.numeric ? 'right' : 'left'}
-                padding={row.disablePadding ? 'none' : 'default'}
+                align={row.numeric ? "right" : "left"}
+                padding={row.disablePadding ? "none" : "default"}
                 sortDirection={orderBy === row.id ? order : false}
               >
                 <Tooltip
                   title="Sort"
-                  placement={row.numeric ? 'bottom-end' : 'bottom-start'}
+                  placement={row.numeric ? "bottom-end" : "bottom-start"}
                   enterDelay={300}
                 >
                   <TableSortLabel
@@ -98,7 +96,7 @@ class EnhancedTableHead extends React.Component {
                 </Tooltip>
               </TableCell>
             ),
-            this,
+            this
           )}
         </TableRow>
       </TableHead>
@@ -112,32 +110,32 @@ EnhancedTableHead.propTypes = {
   onSelectAllClick: PropTypes.func.isRequired,
   order: PropTypes.string.isRequired,
   orderBy: PropTypes.string.isRequired,
-  rowCount: PropTypes.number.isRequired,
+  rowCount: PropTypes.number.isRequired
 };
 
 const toolbarStyles = theme => ({
   root: {
-    paddingRight: theme.spacing.unit,
+    paddingRight: theme.spacing.unit
   },
   highlight:
-    theme.palette.type === 'light'
+    theme.palette.type === "light"
       ? {
           color: theme.palette.secondary.main,
-          backgroundColor: lighten(theme.palette.secondary.light, 0.85),
+          backgroundColor: lighten(theme.palette.secondary.light, 0.85)
         }
       : {
           color: theme.palette.text.primary,
-          backgroundColor: theme.palette.secondary.dark,
+          backgroundColor: theme.palette.secondary.dark
         },
   spacer: {
-    flex: '1 1 100%',
+    flex: "1 1 100%"
   },
   actions: {
-    color: theme.palette.text.secondary,
+    color: theme.palette.text.secondary
   },
   title: {
-    flex: '0 0 auto',
-  },
+    flex: "0 0 auto"
+  }
 });
 
 let EnhancedTableToolbar = props => {
@@ -146,7 +144,7 @@ let EnhancedTableToolbar = props => {
   return (
     <Toolbar
       className={classNames(classes.root, {
-        [classes.highlight]: numSelected > 0,
+        [classes.highlight]: numSelected > 0
       })}
     >
       <div className={classes.title}>
@@ -156,7 +154,7 @@ let EnhancedTableToolbar = props => {
           </Typography>
         ) : (
           <Typography variant="h6" id="tableTitle">
-            Nutrition
+            Leaderboard{" "}
           </Typography>
         )}
       </div>
@@ -182,41 +180,41 @@ let EnhancedTableToolbar = props => {
 
 EnhancedTableToolbar.propTypes = {
   classes: PropTypes.object.isRequired,
-  numSelected: PropTypes.number.isRequired,
+  numSelected: PropTypes.number.isRequired
 };
 
 EnhancedTableToolbar = withStyles(toolbarStyles)(EnhancedTableToolbar);
 
 const styles = theme => ({
   root: {
-    width: '100%',
+    width: "80%",
     marginTop: theme.spacing.unit * 3,
+    maxWidth: "80%"
   },
   table: {
-    minWidth: 1020,
+    minWidth: 1020
   },
   tableWrapper: {
-    overflowX: 'auto',
-  },
+    overflowX: "auto"
+  }
 });
 
 class EnhancedTable extends React.Component {
   state = {
-    order: 'asc',
-    orderBy: 'calories',
+    order: "asc",
+    orderBy: "passes",
     selected: [],
-    data: [
-    ],
+    data: [],
     page: 0,
-    rowsPerPage: 5,
+    rowsPerPage: 5
   };
 
   handleRequestSort = (event, property) => {
     const orderBy = property;
-    let order = 'desc';
+    let order = "desc";
 
-    if (this.state.orderBy === property && this.state.order === 'desc') {
-      order = 'asc';
+    if (this.state.orderBy === property && this.state.order === "desc") {
+      order = "asc";
     }
 
     this.setState({ order, orderBy });
@@ -244,7 +242,7 @@ class EnhancedTable extends React.Component {
     } else if (selectedIndex > 0) {
       newSelected = newSelected.concat(
         selected.slice(0, selectedIndex),
-        selected.slice(selectedIndex + 1),
+        selected.slice(selectedIndex + 1)
       );
     }
 
@@ -264,7 +262,8 @@ class EnhancedTable extends React.Component {
   render() {
     const { classes } = this.props;
     const { data, order, orderBy, selected, rowsPerPage, page } = this.state;
-    const emptyRows = rowsPerPage - Math.min(rowsPerPage, data.length - page * rowsPerPage);
+    const emptyRows =
+      rowsPerPage - Math.min(rowsPerPage, data.length - page * rowsPerPage);
 
     return (
       <Paper className={classes.root}>
@@ -294,16 +293,10 @@ class EnhancedTable extends React.Component {
                       key={n.id}
                       selected={isSelected}
                     >
-                      <TableCell padding="checkbox">
-                        <Checkbox checked={isSelected} />
-                      </TableCell>
                       <TableCell component="th" scope="row" padding="none">
                         {n.name}
                       </TableCell>
-                      <TableCell align="right">{n.calories}</TableCell>
-                      <TableCell align="right">{n.fat}</TableCell>
-                      <TableCell align="right">{n.carbs}</TableCell>
-                      <TableCell align="right">{n.protein}</TableCell>
+                      <TableCell align="right">{n.passes}</TableCell>
                     </TableRow>
                   );
                 })}
@@ -322,10 +315,10 @@ class EnhancedTable extends React.Component {
           rowsPerPage={rowsPerPage}
           page={page}
           backIconButtonProps={{
-            'aria-label': 'Previous Page',
+            "aria-label": "Previous Page"
           }}
           nextIconButtonProps={{
-            'aria-label': 'Next Page',
+            "aria-label": "Next Page"
           }}
           onChangePage={this.handleChangePage}
           onChangeRowsPerPage={this.handleChangeRowsPerPage}
@@ -336,7 +329,7 @@ class EnhancedTable extends React.Component {
 }
 
 EnhancedTable.propTypes = {
-  classes: PropTypes.object.isRequired,
+  classes: PropTypes.object.isRequired
 };
 
 export default withStyles(styles)(EnhancedTable);
