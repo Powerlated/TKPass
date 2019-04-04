58a4deacd92c97a3b0dd1ae31cfabeea83a9bcf6
Powerlated <zjia08@gmail.com>
2019-04-04 07:11:39 -0400

    Front page ported from Zucc (React) version of app

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,98 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <md-tabs>
+      <md-tab>
+        <img alt="R6S Logo" id="logo" src="./assets/r6_logo.png">
+        <HomePage/>
+      </md-tab>
+    </md-tabs>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Vue from "vue";
+import { MdButton, MdContent, MdTabs } from "vue-material/dist/components";
+// import 'vue-material/dist/vue-material.min.css'
+
+Vue.use(MdButton);
+Vue.use(MdContent);
+Vue.use(MdTabs);
+
+import HomePage from "./components/HomePage.vue";
 
 export default {
-  name: 'app',
+  name: "app",
   components: {
-    HelloWorld
+    HomePage
   }
-}
+};
 </script>
 
 <style>
+@import url("https://fonts.googleapis.com/css?family=Noto+Sans+KR");
+body {
+  background-color: #1c0e4e;
+}
+
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+
+  min-height: 100vh;
+  font-family: "Noto Sans KR", sans-serif;
+  border-bottom: black;
+
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  font-size: calc(10px + 2vmin);
+  color: pink;
+  text-shadow: -1px -1px 0 rgb(34, 34, 34), 1px -1px 0 rgb(34, 34, 34),
+    -1px 1px 0 rgb(34, 34, 34), 1px 1px 0 rgb(34, 34, 34);
+}
+
+#app p {
+  width: 800px;
+  text-align: left;
+  max-width: 95%;
+  margin-bottom: 40px;
+}
+
+#app h1,
+#app h2,
+#app h3,
+#app h4,
+#app h5,
+#app h6 {
+  margin: 0;
+  text-align: left;
+}
+
+#logo {
+  animation: App-logo-spin infinite 0.02s linear,
+    App-logo-fx infinite 1s cubic-bezier(0.075, 0.82, 0.165, 1);
+  max-width: 100%;
+
+  pointer-events: none;
+  filter: invert(100%) drop-shadow(1px 0px 0 red) drop-shadow(-1px 0px 0 red)
+    drop-shadow(0px 1px 0 red) drop-shadow(0px -1px 0 red);
+}
+
+@keyframes App-logo-spin {
+  0% {
+    transform: rotate(-3deg);
+  }
+
+  50% {
+    transform: rotate(3deg);
+  }
+
+  100% {
+    transform: rotate(-3deg);
+  }
 }
 </style>
---
+++ src/components/HomePage.vue
@@ -0,0 +1,55 @@
+<template>
+  <div class="hello">
+    <h1>TEAM KILL PASS TRACKER</h1>
+    <p>
+      The beginnings of an EPIC Rainbow Six Siege (and other
+      multiplayer game) Team Kill Pass system.
+    </p>
+
+    <h2>Why did I make this?</h2>
+    <p>
+      To be completely
+      <s>hecking</s> honest, I do not know.
+    </p>
+    <p>
+      Actually, to be realer than real, I made this system because I
+      was inspired by a guy named yeboizaidey on Discord to do so. He
+      sorta started a TK Pass "honor system" that just grew into a
+      giant meme. Zaidey also tried to convince me that making this
+      website was all a big waste of time, but I did not listen
+      because I am always up for a challenge.
+    </p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "HelloWorld",
+  props: {
+    msg: String
+  }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+
+.hello {
+  width: 800px;
+  margin: 0 auto;
+}
+</style>
